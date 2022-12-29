fd9c9c0a2839b04a6f6a9c42eb47b3aa499e2dd0
Tonie Bruijns <TonieBruijns@Acknowledge.nl>
2022-12-29 19:19:22 +0100

    fix and clean apy

---
+++ BackEnd/login.py
@@ -0,0 +1,46 @@
+from flask import Flask, session, abort
+from flask_cors import CORS, cross_origin
+from flaskext.mysql import MySQL
+import pymysql
+from flask import jsonify
+import json
+from flask import flash, request
+from passlib.hash import sha512_crypt
+
+app = Flask(__name__)
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+CORS(app)
+
+mysql = MySQL()
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = ''
+app.config['MYSQL_DATABASE_DB'] = 'webshopusers'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
+
+def login():
+    conn = mysql.connect()
+    cursor = conn.cursor(pymysql.cursors.DictCursor)
+    
+    try:
+        _json = request.json
+        _Username = _json['Username']
+        _Password = _json['Password']
+        
+        if _Username and _Password:
+            cursor.execute('SELECT * FROM users WHERE Username = %s', _Username)
+            account = cursor.fetchone()
+            
+        if account:
+            if sha512_crypt.verify(_Password, account['Password']):
+                response = jsonify('SUCCES')
+                response.status_code = 200
+                return response 
+        else: 
+            response = jsonify('Foutieve gebruikersnaam en / of wachtwoord!')
+            return response
+    except Exception as e:
+            print(e)
+    finally:
+        cursor.close() 
+        conn.close()  
